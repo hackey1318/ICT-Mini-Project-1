a817e004d6f6b7c703598fb27399d87e1cfeeab4
jungjunginwoo <dlsdnjung@gmail.com>
2025-02-17 18:41:16 +0900

    feat: ERD 브랜치에 MemberVO 파일 추가

---
+++ runnerSquare/src/main/java/com/ict/rs/vo/MemberVO.java
@@ -0,0 +1,27 @@
+package com.ict.rs.vo;
+
+public class MemberVO {
+	public int no; 
+	public String memberid;
+	public String memberpwd;
+	public String membername;
+	public String membernickname;
+	public String memberrole;
+	
+	public String email;
+	public String tel;
+	public String tel1;
+	public String tel2;
+	public String tel3;
+	public String zipcode;
+	public String addr;
+	
+	public String birth;
+	public String gender;
+	public String preferpace;
+	public String status;
+	public String createdaccountdate;
+	public String updatedaccountdate;
+	public String latestpwdupdateddate;
+	
+}
