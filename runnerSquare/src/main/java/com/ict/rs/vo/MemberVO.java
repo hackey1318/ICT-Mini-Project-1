e37b09c612e2e4b33aed0f08266f6245e4a257eb
jungjunginwoo <dlsdnjung@gmail.com>
2025-02-17 19:25:06 +0900

    memverVO 객체 annotation 추가

--- runnerSquare/src/main/java/com/ict/rs/vo/MemberVO.java
+++ runnerSquare/src/main/java/com/ict/rs/vo/MemberVO.java
@@ -1,27 +1,33 @@
 package com.ict.rs.vo;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class MemberVO {
 	public int no; 
-	public String memberid;
-	public String memberpwd;
-	public String membername;
-	public String membernickname;
-	public String memberrole;
+	public String id;
+	public String pw;
+	public String name;
+	public String nickName;
+	public String role;	// 권한
 	
 	public String email;
 	public String tel;
 	public String tel1;
 	public String tel2;
 	public String tel3;
-	public String zipcode;
 	public String addr;
 	
 	public String birth;
 	public String gender;
-	public String preferpace;
-	public String status;
-	public String createdaccountdate;
-	public String updatedaccountdate;
-	public String latestpwdupdateddate;
+	public String preferPace;	// 선호 페이스
+	public String status;	// 상태
+	public String createdAt;	// 생성일
+	public String updatedAt;	// 수정일
+	public String passwordLastModifiedAt;	// 패스워드 변경일
 	
 }
