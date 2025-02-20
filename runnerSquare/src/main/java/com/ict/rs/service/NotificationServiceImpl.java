9eed4cd345c098b61049c7b40a0782b5dab177e4
yesim <hackey1318@naver.com>
2025-02-20 15:00:07 +0900

    사용자 알림 조회 API

--- runnerSquare/src/main/java/com/ict/rs/service/NotificationServiceImpl.java
+++ runnerSquare/src/main/java/com/ict/rs/service/NotificationServiceImpl.java
@@ -2,8 +2,11 @@ package com.ict.rs.service;
 
 import com.ict.rs.dao.NotificationDAO;
 import com.ict.rs.vo.MemberVO;
+import com.ict.rs.vo.NotificationVO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -14,11 +17,26 @@ public class NotificationServiceImpl implements NotificationService {
     @Override
     public int getNotificationCount(String userId) {
 
-        // TODO : 회원 조회 기능 추가 후 연결
-        // MemberVO user = this.getMember(userId);
-        MemberVO user = new MemberVO();
-        user.setNo(1);
+        MemberVO user = this.getMember(userId);
         return notificationDAO.getReadableNotificationCountForUser(user.getNo());
     }
 
+    @Override
+    public List<NotificationVO> getNotificationList(String userId) {
+
+        MemberVO user = this.getMember(userId);
+        return notificationDAO.getReadableNotificationListForUser(user.getNo());
+    }
+
+    private MemberVO getMember(String userId) {
+
+        // TODO : 회원 조회 기능 추가 후 연결
+        // MemberVO user = memberService.getUser(userId);
+        MemberVO user = new MemberVO();
+        user.setNo(1);
+        if (user == null) {
+            throw new RuntimeException("사용자가 없습니다.");
+        }
+        return user;
+    }
 }
