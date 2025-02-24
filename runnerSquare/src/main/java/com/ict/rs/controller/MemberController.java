f443f0b9914864c072a7f2274c408658c07cbb24
jungjunginwoo <dlsdnjung@gmail.com>
2025-02-24 10:22:42 +0900

    로그인 API, 빠트린 MemberController,Mapper 추가

--- runnerSquare/src/main/java/com/ict/rs/controller/MemberController.java
+++ runnerSquare/src/main/java/com/ict/rs/controller/MemberController.java
@@ -1,19 +1,31 @@
 package com.ict.rs.controller;
 
 import com.ict.rs.service.MemberService;
+
 import com.ict.rs.vo.MemberVO;
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/users")
-public class MemberContoller {
+public class MemberController {
 
     private final MemberService memberService;
 
@@ -22,4 +34,18 @@ public class MemberContoller {
 
         return memberService.registerMember(vo);
     }
+    
+    @PostMapping("/loginOk")
+    public MemberVO loginOk(@RequestParam String id, @RequestParam String pw, HttpSession session ) {
+    	MemberVO vo = memberService.loginOk(id, pw);
+    	
+    	if(vo != null) {
+    		session.setAttribute("logId", vo.getId());
+    		session.setAttribute("logName", vo.getName());
+    		session.setAttribute("logStatus", "Y");
+    		
+    	}
+    	return vo;
+    }
 }
+    
